4a9b3f82e45e8956f658689de4b5f4b3dcf878f9
Miguel do Rosario <migueldorosario2@gmail.com>
2026-05-16 21:22:18 -0300

    Fix astro site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/discover_brazil',
-	site: 'https://example.com',
+	site: 'https://discoverbrazil.news',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
